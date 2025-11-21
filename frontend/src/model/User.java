0e64a294a7bdf78c4fdd47a2e21130d360507140
Chis <chitran06052005@gmail.com>
2025-11-21 22:24:45 +0700

    Pop up profile implementation

--- frontend/src/model/User.java
+++ frontend/src/model/User.java
@@ -5,7 +5,11 @@ import java.util.Random;
 public class User {
     String initials;
     String name;
+    String gender;
     String role;
+    String address;
+    String birthday;
+    String email;
     boolean is_active;
     String last_msg;
     Random rand = new Random();
@@ -13,7 +17,11 @@ public class User {
     public User(){
         this.initials = "A";
         this.name = "Alice";
+        this.gender = "female";
         this.role = "user";
+        this.birthday = "1/1/2000";
+        this.address = "Here";
+        this.email = "alice@gmail.com";
         this.is_active = rand.nextInt(2) % 2 == 0;
         this.last_msg = "Hello";
     }
@@ -22,6 +30,10 @@ public class User {
         this.initials = name.substring(0,1).toUpperCase();
         this.name = name;
         this.role = "user";
+        this.gender = rand.nextInt(2) % 2 == 0 ? "male" : "female";
+        this.address = "There";
+        this.birthday = rand.nextInt(30) + "//" + rand.nextInt(12) + "//" + rand.nextInt(2025);
+        this.email = name + "@gmail.com";
         this.is_active = rand.nextInt(2) % 2 == 0;
         this.last_msg = "Hello, my name is" + name;
     }
@@ -42,6 +54,19 @@ public class User {
         return is_active;
     }
 
-    // getters + setters
-    // Optional: override toString for debugging
+    public String getBirthDay() {
+        return this.birthday;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
